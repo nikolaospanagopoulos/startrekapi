6f7b610c6079904fbce9d773afa2c9a58e23f911
NikosPanagopoulos <70824044+NikosPanagopoulos@users.noreply.github.com>
2021-01-07 02:06:32 +0200

    connected to database

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 
-//import routes
-import movieRoutes from "./routes/movieRoutes.js";
-
 // TODO: LOAD ENV VARIABLES
 dotenv.config({ path: "./config/config.env" });
 
 //import middleware
 import morgan from "morgan";
+
+//import database config
+import { connectDB } from "./config/db.js";
+
+//import routes
+import movieRoutes from "./routes/movieRoutes.js";
+
+//connect to database
+connectDB();
 
 const app = express();
 
@@ -22,9 +28,17 @@ app.use("/api/v1/movies", movieRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `server running in port ${PORT} in ${process.env.NODE_ENV} environment`
   )
 );
+
+//handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`unhandled rejection : ${err.message}`);
+  //close server exit process
+  server.close(() => process.exit(1));
+});
